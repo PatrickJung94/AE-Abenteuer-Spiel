bffc47699fcd94d56ac6a737a3d7de773dde79f0
till9292 <till9292@googlemail.com>
2021-04-29 13:00:59 +0200

    update Jokers

--- src/wwae/SpielPanel.java
+++ src/wwae/SpielPanel.java
@@ -1,5 +1,6 @@
 package wwae;
 import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
@@ -13,16 +14,22 @@ public class SpielPanel {
 	JFrame window = new JFrame("Wer wird AEllionär");
 
 	JPanel answers = new JPanel();
-	JPanel questionPanel = new JPanel();
+	JPanel infoPanel = new JPanel();
 	JLabel questionLabel = new JLabel();
 	JPanel listenPanel = new JPanel();
+	JPanel jokerPanel = new JPanel();
+	JPanel leiterPanel = new JPanel();
 
 	String[] answerPossibilities = new String[4];
 	String question = new String();
 	
 	Font  f = new Font(Font.SERIF,Font.BOLD, 50);
-	
 	BoxLayout boxLayout = new BoxLayout(listenPanel, BoxLayout.Y_AXIS);
+	GridLayout boxLayoutJoker = new GridLayout(4,1);
+	JButton joker50 = new JButton("50/50");
+	JButton jokerTelefon = new JButton("Telefonjoker");
+	JButton jokerPublikum = new JButton("Publikumsjoker");
+	JButton jokerZusatz = new JButton("Zusatzjoker");
 	
 	
 	
@@ -33,6 +40,7 @@ public class SpielPanel {
 	
 	
 	private void init() {
+		
 		
 		setAnswerPossibilities();
 		setQuestion();
@@ -46,13 +54,20 @@ public class SpielPanel {
 		
 		
 		questionLabel.setFont(f);
-		questionPanel.setLayout(new BorderLayout());
+		infoPanel.setLayout(new BorderLayout());
 		
-		questionPanel.add(questionLabel, BorderLayout.SOUTH);
+		infoPanel.add(jokerPanel, BorderLayout.WEST);
+		infoPanel.add(leiterPanel, BorderLayout.EAST);
+		infoPanel.add(questionLabel, BorderLayout.SOUTH);
 		
+		jokerPanel.setLayout(boxLayoutJoker);
+		jokerPanel.add(joker50);
+		jokerPanel.add(jokerTelefon);
+		jokerPanel.add(jokerPublikum);
+		jokerPanel.add(jokerZusatz);
 		listenPanel.setLayout(boxLayout);
 		listenPanel.add(Box.createRigidArea(new Dimension(3,1)));
-		listenPanel.add(questionPanel);
+		listenPanel.add(infoPanel);
 		listenPanel.add(answers);
 			
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE );
@@ -60,29 +75,29 @@ public class SpielPanel {
 		window.setSize(1280, 720);
 		window.setVisible(true);
 		window.setResizable(false);
+		joker50.setPreferredSize(new Dimension(1000,1000));
 	}
 	
 	
 	public void setAnswerPossibilities(){
 		// später an DB anbinden, aktuell noch Mockdaten
 		
-		answerPossibilities[0] = "A: Das könnte richtig sein";
-		answerPossibilities[1] = "B: Das könnte richtig sein";
-		answerPossibilities[2] = "C: Das könnte richtig sein";
-		answerPossibilities[3] = "D: Das könnte richtig sein";
+		answerPossibilities[0] = "A: Anwendungsschicht";
+		answerPossibilities[1] = "B: Sicherungsschicht";
+		answerPossibilities[2] = "C: Bitübertragungsshicht";
+		answerPossibilities[3] = "D: Transportschicht";
 	}
 	
 	public void setQuestion() {
 		// später an DB anbinden, aktuell noch Mockdaten
 		
-		this.question = "Das ist eine tolle Frage";
+		this.question = "         Wie lautet die zweite Schicht des OSI-Models ? ";
 		this.questionLabel.setText(question);
 	}
 	
 	
 	public static void main(String[] args) {
 		SpielPanel spielpanel = new SpielPanel();
-	
 	}
 	
 	
