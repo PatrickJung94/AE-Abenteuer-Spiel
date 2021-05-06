e4c875e78b86f986fcd7a0d456ce8d29fab4f524
ChrisHei <4ll1h.ch@gmail.com>
2021-05-06 12:01:10 +0200

    Files can now be created, files are stored in data, output.json can be used for testing

--- src/wwae/FileSystem.java
+++ src/wwae/FileSystem.java
@@ -5,10 +5,8 @@
  */
 package wwae;
 
-import java.io.File;
 import java.io.FileWriter;
 import org.json.simple.*;
-
 import java.io.IOException;
 
 /**
@@ -17,20 +15,44 @@ import java.io.IOException;
  *  by CHeinrichs
  */
 public class FileSystem {
-    public void main(String[] args) {
+    /**
+     * Class constructor
+     */
+    public FileSystem() {
         this.createBundleFile("test");
     }
 
+    /**
+     * Function to create a new Questionbundle-File
+     * @param bundleName Name for the File
+     * 
+     * @throws IOException if File couldn't be created
+     */
     public void createBundleFile(String bundleName/* , Question questionObj */) {
-        try {
-            FileWriter file;
+        JSONObject jFileObj = new JSONObject();
 
-            JSONObject jFileObj = new JSONObject();
-
-            
+        JSONObject jQuestionObj = new JSONObject();
+        JSONArray jQuestionArray = new JSONArray();
+        
+        jQuestionObj.put("Frage 1", "Test 1?");
+        jQuestionObj.put("Frage 2", "Test 2?");
+        jQuestionObj.put("Frage 3", "Test 3?");
+        jQuestionObj.put("Frage 4", "Test 4?");
+        
+        jQuestionArray.add(jQuestionObj);
+        
+        jFileObj.put("Fragen", jQuestionArray);
+        
+        try{
+            FileWriter file = new FileWriter("data/output.json");
+            file.write(jFileObj.toJSONString());
+            file.close();
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+        System.out.println("JSON file created: "+jFileObj.toJSONString());
+        System.out.println("JSON file created: "+jFileObj);
     }
 }
+//Gruß an Markus :D
