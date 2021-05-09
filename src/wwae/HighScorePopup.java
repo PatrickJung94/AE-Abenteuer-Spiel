4d1bf5454eeee9599e05421199aefee7589f3a1a
Omar <Omar2tak@gmail.com>
2021-05-09 14:56:03 +0200

    Create HighScorePopup class

---
+++ src/wwae/HighScorePopup.java
@@ -0,0 +1,16 @@
+package wwae;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+import javax.swing.border.AbstractBorder;
+
+//import javax.swing.JList;
+public class HighScorePopup {
+	
+}
