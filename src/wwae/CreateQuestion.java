02a32b5763586d672c217848a97c6a92624d8336
Patrick Jung <patrick.jung94@gmail.com>
2021-05-12 13:05:32 +0200

    updated layout in menu panel reorganized code added all answer inputs

--- src/wwae/CreateQuestion.java
+++ src/wwae/CreateQuestion.java
@@ -29,91 +29,174 @@ public class CreateQuestion extends JFrame {
 		menuPanel.setLayout(new GridLayout(5, 2));
 		
 		JPanel menuBlockTop = new JPanel();
-//		menuBlockTop.setLayout(new GridBagLayout());
-		menuBlockTop.setBackground(new Color(255,0,0));
+		menuBlockTop.setBackground(new Color(220,220,220));
 		menuBlockTop.setPreferredSize(new Dimension(400, 75));
+
+		JLabel title = new JLabel("Erstellen sie eine Frage");
+		Font currentFont = title.getFont();
+		title.setFont(
+			new Font(currentFont.getName(),
+			currentFont.getStyle(),
+			currentFont.getSize() * 2
+		));
+		title.setPreferredSize(new Dimension(300, 50));
+		JPanel titlePanel = new JPanel();
+		titlePanel.setBackground(new Color(220,220,220));
+		titlePanel.add(title);
+		menuBlockTop.add(titlePanel);
 		
 		JPanel menuBlockBottom = new JPanel();
-//		menuBlockBottom.setLayout(new GridBagLayout());
-		menuBlockBottom.setBackground(new Color(255,0,0));
+		menuBlockBottom.setBackground(new Color(220,220,220));
 		menuBlockBottom.setPreferredSize(new Dimension(400, 75));
+
 		JButton saveButton = new JButton("Speichern");
 		menuBlockBottom.add(saveButton);
-		
-		
+
 		JPanel left = new JPanel();
 		left.setPreferredSize(new Dimension(200, 400));
-		left.setBackground(new Color(0,255,0));
+		left.setBackground(new Color(220,220,220));
 		JPanel right = new JPanel();
 		right.setPreferredSize(new Dimension(200, 400));
-		right.setBackground(new Color(0,255,0));
+		right.setBackground(new Color(220,220,220));
 		
-		
+		// ---------- Create Row for time Input -----------
+
+		JPanel timePanel = new JPanel();
 		JLabel time = new JLabel("Zeit in Sekunden");
-		time.setPreferredSize(new Dimension(150, 75));
+		time.setPreferredSize(new Dimension(150, 40));
+
+		JPanel timeInputPanel = new JPanel();
 		JTextField timeInput = new JTextField();
-		timeInput.setPreferredSize(new Dimension(150, 75));
-		// timeInput.addKeyListener(new KeyListener() {
+		timeInput.setPreferredSize(new Dimension(300, 40));
 
-		// 	@Override
-		// 	public void keyTyped(KeyEvent e) {
-		// 		// TODO Auto-generated method stub
-		// 		newQuestion.setTime(e.getKeyChar());
-		// 		System.out.println(newQuestion.getTime());
-		// 	}
+		timePanel.add(time);
+		timeInputPanel.add(timeInput);
+		menuPanel.add(timePanel);
+		menuPanel.add(timeInputPanel);
 
-		// 	@Override
-		// 	public void keyPressed(KeyEvent e) {
-		// 		// TODO Auto-generated method stub
-				
-		// 	}
+		// ---------- Create Row for question text Input -----------
 
-		// 	@Override
-		// 	public void keyReleased(KeyEvent e) {
-		// 		// TODO Auto-generated method stub
-				
-		// 	}
-
-		// });
-		
-		menuPanel.add(time);
-		menuPanel.add(timeInput);
-
+		JPanel textPanel = new JPanel();
 		JLabel text = new JLabel("Frage: ");
-		text.setPreferredSize(new Dimension(150, 75));
+		text.setPreferredSize(new Dimension(150, 40));
+
+		JPanel textInputPanel = new JPanel();
 		JTextField textInput = new JTextField();
-		textInput.setPreferredSize(new Dimension(150, 75));
-		menuPanel.add(text);
-		menuPanel.add(textInput);
+		textInput.setPreferredSize(new Dimension(300, 40));
 
+		textPanel.add(text);
+		textInputPanel.add(textInput);
+		menuPanel.add(textPanel);
+		menuPanel.add(textInputPanel);
+
+		// ---------- Create Row for correct answer index Input -----------
+
+		JPanel correctIndexPanel = new JPanel();
 		JLabel correctIndex = new JLabel("Richtige Antowrt Index");
-		correctIndex.setPreferredSize(new Dimension(150, 75));
+		correctIndex.setPreferredSize(new Dimension(150, 40));
+		
+		JPanel correctIndexInputPanel = new JPanel();
 		JTextField correctIndexInput = new JTextField();
-		correctIndexInput.setPreferredSize(new Dimension(150, 75));
-		menuPanel.add(correctIndex);
-		menuPanel.add(correctIndexInput);
+		correctIndexInput.setPreferredSize(new Dimension(300, 40));
 
+		correctIndexPanel.add(correctIndex);
+		correctIndexInputPanel.add(correctIndexInput);
+		menuPanel.add(correctIndexPanel);
+		menuPanel.add(correctIndexInputPanel);
+
+		// ---------- Create Row for answer Inputs -----------
+
+		JPanel antwortenPanel = new JPanel();
 		JLabel antworten = new JLabel("Antworten");
-		antworten.setPreferredSize(new Dimension(150, 75));
-		JTextField antwortenInput = new JTextField();
-		antwortenInput.setPreferredSize(new Dimension(150, 75));
-		menuPanel.add(antworten);
-		menuPanel.add(antwortenInput);
+		antworten.setPreferredSize(new Dimension(150, 40));
+		
+		JPanel antwortenInputPanel = new JPanel();
+		antwortenInputPanel.setPreferredSize(new Dimension(400, 100));
+		antwortenInputPanel.setLayout(new GridLayout(4, 2));
 
-		JLabel textForPhoneJoker = new JLabel("Ja also ich denke das antwort B richtig ist. Davon habe ich schonmal geh�rt");
-		textForPhoneJoker.setPreferredSize(new Dimension(150, 75));
+		JPanel antwort1TextPanel = new JPanel();
+		JLabel antwort1Text = new JLabel("Antwort 1");
+		antwort1Text.setPreferredSize(new Dimension(100, 20));
+		
+		antwort1TextPanel.add(antwort1Text);
+		JPanel antwort1Panel = new JPanel();
+		JTextField antwort1Input = new JTextField();
+		antwort1Input.setPreferredSize(new Dimension(200, 20));
+		antwort1Panel.add(antwort1Input);
+		antwortenInputPanel.add(antwort1TextPanel);
+		antwortenInputPanel.add(antwort1Panel);
+
+		JPanel antwort2TextPanel = new JPanel();
+		JLabel antwort2Text = new JLabel("Antwort 2");
+		antwort2Text.setPreferredSize(new Dimension(100, 20));
+		
+		antwort2TextPanel.add(antwort2Text);
+		JPanel antwort2Panel = new JPanel();
+		JTextField antwort2Input = new JTextField();
+		antwort2Input.setPreferredSize(new Dimension(200, 20));
+		antwort2Panel.add(antwort2Input);
+		antwortenInputPanel.add(antwort2TextPanel);
+		antwortenInputPanel.add(antwort2Panel);
+
+		JPanel antwort3TextPanel = new JPanel();
+		JLabel antwort3Text = new JLabel("Antwort 3");
+		antwort3Text.setPreferredSize(new Dimension(100, 20));
+		
+		antwort3TextPanel.add(antwort3Text);
+		JPanel antwort3Panel = new JPanel();
+		JTextField antwort3Input = new JTextField();
+		antwort3Input.setPreferredSize(new Dimension(200, 20));
+		antwort3Panel.add(antwort3Input);
+		antwortenInputPanel.add(antwort3TextPanel);
+		antwortenInputPanel.add(antwort3Panel);
+
+		JPanel antwort4TextPanel = new JPanel();
+		JLabel antwort4Text = new JLabel("Antwort 4");
+		antwort4Text.setPreferredSize(new Dimension(100, 20));
+		
+		antwort4TextPanel.add(antwort4Text);
+		JPanel antwort4Panel = new JPanel();
+		JTextField antwort4Input = new JTextField();
+		antwort4Input.setPreferredSize(new Dimension(200, 20));
+		antwort4Panel.add(antwort4Input);
+		antwortenInputPanel.add(antwort4TextPanel);
+		antwortenInputPanel.add(antwort4Panel);
+
+		antwortenPanel.add(antworten);
+
+		menuPanel.add(antwortenPanel);
+		menuPanel.add(antwortenInputPanel);
+
+		// ---------- Create Row for telephon joker Input -----------
+
+		JPanel textForPhoneJokerPanel = new JPanel();
+		JLabel textForPhoneJoker = new JLabel("Text für den Telefon Joker");
+		textForPhoneJoker.setPreferredSize(new Dimension(150, 40));
+
+		JPanel textForPhoneJokerInputPanel = new JPanel();
 		JTextField textForPhoneJokerInput = new JTextField();
-		textForPhoneJokerInput.setPreferredSize(new Dimension(150, 75));
-		menuPanel.add(textForPhoneJoker);
-		menuPanel.add(textForPhoneJokerInput);
+		textForPhoneJokerInput.setPreferredSize(new Dimension(300, 40));
+
+		textForPhoneJokerPanel.add(textForPhoneJoker);
+		textForPhoneJokerInputPanel.add(textForPhoneJokerInput);
+		menuPanel.add(textForPhoneJokerPanel);
+		menuPanel.add(textForPhoneJokerInputPanel);
+
+		// ---------- Create save button action -----------
 
 		saveButton.addActionListener((event) -> {
 			newQuestion.setTime(Double.parseDouble(timeInput.getText()));
 			newQuestion.setText(textInput.getText());
 			newQuestion.setCorrectIndex(Integer.parseInt(correctIndexInput.getText()));
-			String[] temp = {antwortenInput.getText()};
+			String[] temp = {
+				antwort1Input.getText(),
+				antwort2Input.getText(),
+				antwort3Input.getText(),
+				antwort4Input.getText()
+			};
 			newQuestion.setAnswers(temp);
 			newQuestion.setTextForPhoneJoker(textForPhoneJokerInput.getText());
+
 			System.out.println(newQuestion.getCorrectIndex());
 			System.out.println(newQuestion.getTime());
 			System.out.println(newQuestion.getText());
