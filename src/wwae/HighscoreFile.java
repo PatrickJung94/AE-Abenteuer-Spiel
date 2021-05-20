6ff6ae5d491e5fa7e6d6553ee45facb5557de2ed
Markus Bachmann <lutzhemmer123@gmail.com>
2021-05-20 12:38:10 +0200

    initial wip

---
+++ src/wwae/HighscoreFile.java
@@ -0,0 +1,20 @@
+package wwae;
+
+// TODO: addEntryToFile
+// TODO: getHighscoresAsArray
+
+import java.io.*;
+import java.io.FileReader;
+import java.io.FileNotFoundException;    
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.ArrayList;
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import static java.nio.file.StandardOpenOption.*;
+
+public class HighscoreFile {
+ // TODO
+}
+
